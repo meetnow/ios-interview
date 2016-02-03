0bbaaaf685fc3ad2bd1ebe187bc8f3e47af0481c
sebastianmeetnow <sebastian.otto@meetnow.eu>
2016-02-03 15:17:05 +0700

    Update Test01.m

--- ios interview/ios interviewTests/Test01.m
+++ ios interview/ios interviewTests/Test01.m
@@ -28,7 +28,7 @@
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
     
-    XCTAssertEqual(4, 2+1);
+    XCTAssertEqual(4, 2+2);
 }
 
 - (void)testPerformanceExample {
